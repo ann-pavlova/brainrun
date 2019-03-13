d5b1548b9bad9511eecaff0ce51fd0b81148ff35
Ann Pavlova <ann_pavlova@outlook.com>
2019-03-13 21:04:54 +0300

    Change api route

--- src/store/routes.js
+++ src/store/routes.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://192.168.1.12:8104';
+export const BASE_URL = 'https://brainrun.beatcode.net';
 export const EDIT_QUESTION = 'editQuestion';
 export const QUESTIONS = 'questions';
 export const TAGS = 'tags';
